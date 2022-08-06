93168958e7c0234df297562326f39243d146d677
ling <1137674651@qq.com>
2022-08-06 17:49:21 +0800

    初始

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,13 +1,50 @@
+import styled from '@emotion/styled'
 import { useAuth } from 'context/auth-context'
 import React from 'react'
 import ProjectList from 'screens/projectList/ProjectList'
 
 export default function AuthenticatedApp() {
-    const {logout}=useAuth()
+  const { logout } = useAuth()
   return (
     <div>
-        <button onClick={logout}>登出</button>
-        <ProjectList/>
+      <Header>
+        <HeaderLeft>
+          <h3>logo</h3>
+          <h3>项目</h3>
+          <h3>用户</h3>
+        </HeaderLeft>
+        <HeaderRight>
+          <button onClick={logout}>登出</button>
+        </HeaderRight>
+      </Header>
+      <Main>
+        <ProjectList />
+      </Main>
     </div>
   )
 }
+
+
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 6rem 1fr;
+  height: 100vh;
+`;
+
+const Header = styled.header`
+  grid-area: header;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderLeft = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const HeaderRight = styled.div``;
+
+const Main = styled.main`
+  grid-area: main;
+`
