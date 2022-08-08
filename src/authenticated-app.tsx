345b0ec441922da4c0ebe88d80d1d504fdbd8f99
ling <1137674651@qq.com>
2022-08-08 15:22:22 +0800

    emotion组件

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import React from 'react'
 import ProjectList from 'screens/projectList/ProjectList'
@@ -7,8 +8,8 @@ export default function AuthenticatedApp() {
   const { logout } = useAuth()
   return (
     <div>
-      <Header>
-        <HeaderLeft>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
           <h3>logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
@@ -31,20 +32,13 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
+const Header = styled(Row)`
 `;
 
-const HeaderLeft = styled.div`
-  display: flex;
-  align-items: center;
-`;
+//引入自定义样式组件,注意千万不要加.div，会导致运行不了且不报错
+const HeaderLeft = styled(Row)``;
+
 const HeaderRight = styled.div``;
 
 const Main = styled.main`
-  grid-area: main;
 `
--- src/components/lib.tsx
+++ src/components/lib.tsx
@@ -1,6 +1,19 @@
 import styled from "@emotion/styled";
+//import styled from "@emotion/styled/macro";
 
-export const Row = styled.div`
+//<>中写要传入的参数
+export const Row = styled.div<{
+    gap?: number | boolean
+    between?: boolean
+    marginBottom?: number
+}>`
 display: flex;
 align-items: center;
+justify-content: ${props => props.between ? 'space-between' : undefined};
+margin-bottom: ${props => props.marginBottom + 'rem'};
+>*{
+    margin-top: 0 !important;
+    margin-bottom: 0 !important;
+    margin-right: ${props => typeof props.gap === 'number' ? props.gap +'rem' : props.gap ? '2rem' : undefined};
+}
 `
